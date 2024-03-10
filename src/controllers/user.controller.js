7285715b80f17a21fc048a363f93bc62636521e0
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2024-03-10 14:05:01 +0600

    file upload successfully to cludinary and save data on database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,27 +3,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
 const registerUser = asyncHandler(async (req, res) => {
-  const { fullName, email, password, userName } = req.body;
-  res.status(200).json({
-    success: true,
-    fullName,
-    email,
-    password,
-    userName,
-  });
+  const { fullName, email, userName, password } = req.body;
+  console.log(req.body);
+
   if (
-    [fullName, email, password, userName].some((field) => field?.trim() === "")
+    [fullName, email, userName, password].some((field) => field?.trim() === "")
   ) {
-    throw new ApiError(400, "fullName is required");
+    throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
-    $or: [{ email: email }, { userName: userName }],
+  const existedUser = await User.findOne({
+    $or: [{ userName }, { email }],
   });
-  if (existedUser) {
-    throw new ApiError(409, "User already exists");
-  }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImagePath = req.files?.coverImage[0]?.path;
@@ -32,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImagePath);
+
   if (!avatar) {
     throw new ApiError(400, "avatar is required");
   }
@@ -40,21 +34,22 @@ const registerUser = asyncHandler(async (req, res) => {
     fullName,
     email,
     password,
-    userName: userName.toLowerCase(),
+    userName,
     avatar: avatar.url,
-    coverImage: coverImage?.url || "",
+    coverImage: coverImage.url || null,
   });
 
-  const createdUser = await User.findById(user_id).select(
+  const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
 
   if (!createdUser) {
-    throw new ApiError(500, "Internal server error while creating user");
+    throw new ApiError(500, "Something went wrong while registering the user");
   }
+
   return res
     .status(201)
-    .json(new ApiResponse(200, "User register successfully"));
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export default registerUser;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,25 +1,26 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 const uploadOnCloudinary = async (localFilePath) => {
   try {
-    if (!localFilePath) {
-      const uploadResult = await cloudinary.uploader.upload(localFilePath, {
-        resource_type: "auto",
-      });
-      console.log(
-        `File uploaded successfully on cloudinary ${uploadResult.url}`
-      );
+    if (!localFilePath) return null;
 
-      return uploadResult;
-    }
-  } catch (err) {
-    fs.unlinkSync(localFilePath); // delete the local file path
-    console.log(err);
+    const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(localFilePath);
+    return uploadResult;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
   }
 };
+
 export { uploadOnCloudinary };
