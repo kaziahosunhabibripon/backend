3d7fb1e95d838c6bb8a20c4dda98a40a820e8cbc
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2024-03-10 11:06:51 +0600

    added register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,60 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, password, userName } = req.body;
   res.status(200).json({
-    message: "Welcome OK Ripon",
+    success: true,
+    fullName,
+    email,
+    password,
+    userName,
   });
+  if (
+    [fullName, email, password, userName].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "fullName is required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ email: email }, { userName: userName }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImagePath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImagePath);
+  if (!avatar) {
+    throw new ApiError(400, "avatar is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    userName: userName.toLowerCase(),
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user_id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Internal server error while creating user");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, "User register successfully"));
 });
 
 export default registerUser;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 cloudinary.config({
-  cloud_name: process_params.CLOUDINARY_CLOUD_NAME,
-  api_key: process_params.CLOUDINARY_API_KEY,
-  api_secret: process_params.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 const uploadOnCloudinary = async (localFilePath) => {
   try {
@@ -22,3 +22,4 @@ const uploadOnCloudinary = async (localFilePath) => {
     console.log(err);
   }
 };
+export { uploadOnCloudinary };
