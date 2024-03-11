e766aeabd4758793da66bc1de6a07107477f845d
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2024-03-11 16:54:00 +0600

    login successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,15 +7,17 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
-    const accessToken = user.getAccessToken();
-    const refreshToken = user.getRefreshToken();
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
     user.refreshToken = refreshToken;
     await user.save({ validateBeforeSave: false });
+
     return { accessToken, refreshToken };
   } catch (error) {
     throw new ApiError(
       500,
-      "Something went wrong while generating refresh and access token"
+      "Something went wrong while generating referesh and access token"
     );
   }
 };
@@ -80,8 +82,8 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password, userName } = req.body;
 
-  if (!email || !userName) {
-    throw new ApiError(400, "Username or email must be provided");
+  if (!(email && password)) {
+    throw new ApiError(400, "Email and password must be provided");
   }
   const user = await User.findOne({
     $or: [{ userName }, { email }],
