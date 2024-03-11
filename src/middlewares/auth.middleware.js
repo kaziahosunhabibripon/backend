a8193120bbe998d7dd5f5baa411cea12ec10c220
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2024-03-11 17:29:29 +0600

    access token done

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,13 +3,12 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
 
-export const verifyJWT = asyncHandler(async (req, _res, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    // console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
