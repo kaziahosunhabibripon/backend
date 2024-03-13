d0bb102c386800b542d7eb61645e6c964a04d24c
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2024-03-13 14:46:15 +0600

    subscription models added

---
+++ src/models/subscription.models.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "Channel",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
